1220ba7cf78980c30eab6313d7bb873331aa68bb
jeff.wade <jeff.s.wade@rtx.com>
2026-04-16 11:57:13 -0400

    Add subset_type to control gage/vpu gpkg return

--- src/mswm/build_inputs.py
+++ src/mswm/build_inputs.py
@@ -516,7 +516,8 @@ class RealizationBuilder:
         self.run_type = self.conf1.get("run_type") if self.conf1 else None
         self.domain = self.conf1.get("domain") if self.conf1 else None
         self.environment = self.conf1.get("environment") if self.conf1 else None
-        self.basin = self.conf1['basin'] if self.conf1 else None
+        self.basin = self.conf1.get("basin") if self.conf1 else None
+        self.subset_type = self.conf1.get("subset_type") if self.conf1 else None
 
         # Retrieve module properties inputs
         self.module_prop = self.input_configs.get("ModuleProperties")
@@ -542,7 +543,6 @@ class RealizationBuilder:
         Create input directory to store realization file and BMI config files
         """
         # Set run directory based on run_type
-        self.basin = self.conf1['basin']
         obj_fnc = self.conf2.get('objective_function') or "none"
         opt_alg = self.conf2.get('optimization_algorithm') or "none"
         if self.run_type == 'calibration':
@@ -798,7 +798,7 @@ class RealizationBuilder:
         self.gpkg_file = self.conf3.get('hydrofab_file')
         if self.gpkg_file is None:
             # If gpkg_file not provided, retrieve gpkg from icefabric and save to file
-            self.gpkg_file = gfun.call_icefabric_gpkg(self.basin, self.domain, self.input_dir, self.environment, 'nhf')
+            self.gpkg_file = gfun.call_icefabric_gpkg(self.basin, self.subset_type, self.domain, self.input_dir, self.environment, 'nhf')
 
         else:
             # Ensure user provided geopackage file exists
@@ -1254,10 +1254,10 @@ class RealizationBuilder:
                 gfun.update_fcst_forcing_config(self.cycle_date, self.cycle_hour, self.root_dir, self.forcing_template, gpkg_file, self.forcing_config_dir,
                                                 self.forcing_config_file, self.use_cold_start, self.use_warm_start, self.hind_cycle, self.prev_hind_cycle,
                                                 self.forcing_lag, self.cold_start_datetime, self.fcst_lookback,
-                    scratch_dir_override=self.scratch_dir_override,
-                    input_forcing_dirs_override_root=self.input_forcing_dirs_override_root,
-                    forcing_product_versions=self.forcing_product_versions,
-                )
+                                                scratch_dir_override=self.scratch_dir_override,
+                                                input_forcing_dirs_override_root=self.input_forcing_dirs_override_root,
+                                                forcing_product_versions=self.forcing_product_versions,
+                                                )
             else:
                 # Update historical dynamic parameters in forcing engine configuration file
                 gfun.update_hist_forcing_config(self.time_period, self.root_dir, self.forcing_template, gpkg_file, self.forcing_config_dir, self.forcing_config_file, self.run_type, self.global_domain, self.forcing_static_dir)
@@ -1590,7 +1590,7 @@ class RealizationBuilder:
         """
         Write parallel processing partition file
         """
-        if getattr(self, "_building_fcst_realization", "")  is True:
+        if getattr(self, "_building_fcst_realization", "") is True:
             cat_file = self.gpkg_cats
             partition_config_basename_prefix = "forecast"
             work_dir, sub_dir_name = os.path.split(self.input_dir)
