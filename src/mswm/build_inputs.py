20d9a997e18e5cbc6bd1f943ac94232100ebde2c
jeff.wade <jeff.s.wade@rtx.com>
2026-06-03 15:06:09 -0400

    Add fcst return missing from recent merge

--- src/mswm/build_inputs.py
+++ src/mswm/build_inputs.py
@@ -2056,7 +2056,7 @@ class RealizationBuilder:
         if self.save_state:
             return self.realization_file, self.save_state_to
         else:
-            return self.realization_file
+            return self.realization_file, None
 
     def build_default_realization(self):
         """
