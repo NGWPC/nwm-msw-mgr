6a947370acaf563f0c53c6e16d1cae350b2542d2
jeff.wade <jeff.s.wade@rtx.com>
2026-07-06 16:38:57 -0400

    Update oe icefabric endpoint to http

--- src/mswm/utils/ginputfunc.py
+++ src/mswm/utils/ginputfunc.py
@@ -172,7 +172,7 @@ def call_icefabric_gpkg(
     if environment == 'test':
         url = f"http://edfs.test.nextgenwaterprediction.com/api/v1/hydrofabric/{basin}/gpkg"
     elif environment == 'oe':
-        url = f"https://edfs.oe.nextgenwaterprediction.com/api/v1/hydrofabric/{basin}/gpkg"
+        url = f"http://edfs.oe.nextgenwaterprediction.com/api/v1/hydrofabric/{basin}/gpkg"
 
     # Build query parameters
     params = {"id_type": id_type,
